50a4c6b7c26c6789507bc9ef8bcf93bf92037e75
shubhams3423 <shubham1844s@gmail.com>
2023-12-20 10:24:30 +0530

    Resolved vercel error.

--- src/ProductsColors/ProductsColors.jsx
+++ src/ProductsColors/ProductsColors.jsx
@@ -1,22 +1,13 @@
 import React from "react";
-import { twMerge } from "tailwind-merge";
 const ProductsColors = ({ availableProductColors }) => {
   return (
     <div className="flex items-center justify-evenly">
       {availableProductColors.map((productColor, key) => {
         return (
           <div
-            className={twMerge(
-              "flex h-6 w-6 cursor-pointer items-center justify-center rounded-full border-2 border-solid",
-              `border-${productColor}-200`,
-            )}
+            className={`flex h-6 w-6 cursor-pointer items-center justify-center rounded-full border-2 border-solid border-${productColor}-200`}
           >
-            <p
-              className={twMerge(
-                "h-4 w-4 rounded-full",
-                `bg-${productColor}-500`,
-              )}
-            ></p>
+            <p className={`h-4 w-4 rounded-full bg-${productColor}-500`}></p>
           </div>
         );
       })}
